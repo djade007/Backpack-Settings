371c1d55adc2648f86121be675059a0a50c88ca3
Olajide Afeez <jide4real43@gmail.com>
2016-11-12 12:49:22 +0100

    update migration

--- src/database/seeds/SettingsTableSeeder.php
+++ src/database/seeds/SettingsTableSeeder.php
@@ -7,6 +7,28 @@ use Illuminate\Support\Facades\DB;
 
 class SettingsTableSeeder extends Seeder
 {
+    private $settings;
+
+    /**
+     * SettingsTableSeeder constructor.
+     * @param $settings
+     */
+    public function __construct($settings)
+    {
+        $this->settings = [
+            [
+                'key'           => 'motto',
+                'name'          => 'Motto',
+                'description'   => 'Website motto',
+                'value'         => 'this is the value',
+                'field'         => '{"name":"value","label":"Value", "title":"Motto value" ,"type":"textarea"}',
+                'active'        => 1,
+
+            ]
+        ];
+    }
+
+
     /**
      * Run the database seeds.
      *
@@ -14,43 +36,11 @@ class SettingsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('settings')->insert([
-            'key'           => 'contact_email',
-            'name'          => 'Contact form email address',
-            'description'   => 'The email address that all emails from the contact form will go to.',
-            'value'         => 'admin@updivision.com',
-            'field'         => '{"name":"value","label":"Value","type":"email"}',
-            'active'        => 1,
-        ]);
-
-        DB::table('settings')->insert([
-            'key'           => 'contact_cc',
-            'name'          => 'Contact form CC field',
-            'description'   => 'Email adresses separated by comma, to be included as CC in the email sent by the contact form.',
-            'value'         => '',
-            'field'         => '{"name":"value","label":"Value","type":"email"}',
-            'active'        => 1,
-
-        ]);
-
-        DB::table('settings')->insert([
-            'key'           => 'contact_bcc',
-            'name'          => 'Contact form BCC field',
-            'description'   => 'Email adresses separated by comma, to be included as BCC in the email sent by the contact form.',
-            'value'         => '',
-            'field'         => '{"name":"value","label":"Value","type":"email"}',
-            'active'        => 1,
-
-        ]);
-
-        DB::table('settings')->insert([
-            'key'           => 'motto',
-            'name'          => 'Motto',
-            'description'   => 'Website motto',
-            'value'         => 'this is the value',
-            'field'         => '{"name":"value","label":"Value", "title":"Motto value" ,"type":"textarea"}',
-            'active'        => 1,
-
-        ]);
+        foreach ($this->settings as $setting) {
+            $exists = DB::table('settings')->where('key', $setting->key)->count();
+            if($exists || !$setting['key']) // continue if setting already exists
+                continue;
+            DB::table('settings')->insert($setting);
+        }
     }
 }
